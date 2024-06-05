efb149afc95ef309e2af1cf88661c8234afbfec6
Jeremy Weiss <jlweiss@email.arizona.edu>
2024-06-05 16:56:06 -0700

    improve time series caption text

--- app/R/fxnTimeSeriesCaption.R
+++ app/R/fxnTimeSeriesCaption.R
@@ -80,7 +80,7 @@ fxnTimeSeriesCaption <- function(azmetStation, inData) {
     htmltools::p(
       htmltools::HTML(
         paste0(
-          "The estimated canopy temperature of ", doyValue, " °F (black point) on ", gsub(" 0", " ", format(as.Date(max(inData$datetime)), "%B %d, %Y")), " at the AZMet ", azmetStation, " station indicates ", doyLevel, ". The maximum, average, and minimum of estimated canopy temperatures on ", format(as.Date(max(inData$datetime)), "%B"), " ", format(as.Date(max(inData$datetime)), "%d"), " (vertical dotted line) are ", doyMaxValue, ", ", doyAverageValue, ", and ", doyMinValue, " °F, respectively, based on data from ", doyMinYear, " through ", doyMaxYear, " (black and gray points)."
+          "The estimated canopy temperature of ", doyValue, " °F (black point) on ", gsub(" 0", " ", format(as.Date(max(inData$datetime)), "%B %d, %Y")), " at the AZMet ", azmetStation, " station indicates ", doyLevel, ". The maximum, average, and minimum of estimated canopy temperatures on ", gsub(" 0", " ", format(as.Date(max(inData$datetime)), "%B %d")), " (vertical dotted line) are ", doyMaxValue, ", ", doyAverageValue, ", and ", doyMinValue, " °F, respectively, based on data from ", doyMinYear, " through ", doyMaxYear, " (black and gray points)."
         ),
       ),
       
